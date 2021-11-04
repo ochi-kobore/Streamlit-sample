b2b3a41620e60d22c575b4f628b4a8b9832baa14
Naoki <n9r2t1g0r3s9h@gmail.com>
2021-11-04 20:10:10 +0900

    1st commit

---
+++ main.py
@@ -0,0 +1,35 @@
+import streamlit as st
+import time
+
+st.title('Stremalit 超入門')
+
+st.write('プレグレスバーの表示')
+'Start!!'
+
+latest_iteration = st.empty()
+bar = st.progress(0)
+
+for i in range(100):
+    latest_iteration.text(f'Iteration {i+1}')
+    bar.progress(i + 1)
+    time.sleep(0.1)
+
+'Done!!!'
+
+left_column, right_column = st.beta_columns(2)
+button = left_column.button('右カラムに文字を表示')
+if button:
+    right_column.write('ここは右カラムです')
+
+expander = st.beta_expander('問い合わせ1')
+expander.write('問い合わせ1の回答')
+expander = st.beta_expander('問い合わせ2')
+expander.write('問い合わせ2の回答')
+expander = st.beta_expander('問い合わせ3')
+expander.write('問い合わせ3の回答')
+
+# text = st.text_input('あなたの趣味を教えてください。')
+# condition = st.slider('あなたのいまの調子は？', 0, 100, 50)
+
+# 'あなたの趣味:', text
+# 'コンディション: ', condition
